fd17d1dd80b30dd53eecad308a199342cad2db8f
DevelopJJong <101362306+DevelopJJong@users.noreply.github.com>
2022-06-07 20:58:41 +0900

    [Bronze II] Title: 손익분기점, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        int i=1;
+        if(B>=C){
+            System.out.println(-1);
+        }
+        else {
+            System.out.println((A/(C-B))+1);
+        }
+    }
+}
