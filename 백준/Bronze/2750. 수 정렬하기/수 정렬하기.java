a8a1351e0483fa9086d4eba544b74c6f72eaa850
DevelopJJong <101362306+DevelopJJong@users.noreply.github.com>
2022-06-16 23:20:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 340 ms, Memory: 22480 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        int temp = 0;
-        for(int k=0; k<arr.length; k++){
-            arr[k] = sc.nextInt();
-        }
-        for(int i = 0; i < arr.length; i++) {
-            for(int j= 1 ; j < arr.length-i; j++) {
-                if(arr[j-1] > arr[j]) {
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-        for(int m=0; m<arr.length; m++){
-            System.out.println(arr[m]);
-        }
-    }
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+ 
+		// 정렬 메소드
+		Arrays.sort(arr);
+		
+		for(int val : arr) {
+			System.out.println(val);
+		}
+ 
+	}
 }
