b74088239e6562c8a47f90038edc0a8e9f4012d8
DevelopJJong <101362306+DevelopJJong@users.noreply.github.com>
2022-07-04 21:13:25 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -6,8 +6,6 @@ class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
         int count = 0;
-        int Max = 0;
-        int Min = 0;
         
         ArrayList<Integer> lottos_pick = new ArrayList<>();
         
@@ -25,27 +23,26 @@ class Solution {
         HashSet<Integer> result = new HashSet<>(lottos_pick);
         result.retainAll(win_nums_pick);
         
-        Max = result.size()+count;
-        Min = result.size();
-        if(Max>6){
-            Max = 6;
-        }
-        switch(Max){
-            case 6: answer[0] = 1; break;
-            case 5: answer[0] = 2; break;
-            case 4: answer[0] = 3; break;
-            case 3: answer[0] = 4; break;
-            case 2: answer[0] = 5; break;
-            default: answer[0] = 6; break;
-        }
-        switch(Min){
-            case 6: answer[1] = 1; break;
-            case 5: answer[1] = 2; break;
-            case 4: answer[1] = 3; break;
-            case 3: answer[1] = 4; break;
-            case 2: answer[1] = 5; break;
-            default: answer[1] = 6; break;
-        }
+        answer[0] = rank(result.size()+count);
+        answer[1] = rank(result.size());
+        
+        
         return answer;
     }
+    public int rank(int n){
+        switch(n){
+            case 6:
+                return 1;
+            case 5:
+                return 2;
+            case 4:
+                return 3;
+            case 3:
+                return 4;
+            case 2:
+                return 5;
+            default:
+                return 6;
+        }
+}
 }
