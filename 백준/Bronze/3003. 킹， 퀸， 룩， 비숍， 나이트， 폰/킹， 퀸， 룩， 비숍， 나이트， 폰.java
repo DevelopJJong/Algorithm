4f6ebd5d49d6c067c0d4ff2367a1b01dd3e8aa04
DevelopJJong <101362306+DevelopJJong@users.noreply.github.com>
2023-01-27 10:22:13 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 192 ms, Memory: 16720 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,14 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
 // 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개로 구성되어 있다.
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int king = sc.nextInt();
-        int queen = sc.nextInt();
-        int rook = sc.nextInt();
-        int bishop = sc.nextInt();
-        int knight = sc.nextInt();
-        int pawn = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int king = Integer.parseInt(st.nextToken());
+        int queen = Integer.parseInt(st.nextToken());
+        int rook = Integer.parseInt(st.nextToken());
+        int bishop = Integer.parseInt(st.nextToken());
+        int knight = Integer.parseInt(st.nextToken());
+        int pawn = Integer.parseInt(st.nextToken());
+        
         System.out.print((1-king)+" "+(1-queen)+" "+(2-rook)+" "+(2-bishop)+" "+(2-knight)+" "+(8-pawn)); //
     }
 }
